368589180825fe0dd5607d2a4aabd7ecca8caf92
bleung329 <bleung@stuy.edu>
2018-03-01 03:09:57 -0500

    init commit

---
+++ svg.js
@@ -0,0 +1,31 @@
+console.log("Hi my name is JS!")
+var pic = document.getElementById("vimage")
+
+
+
+var change = function(e)
+{
+	e.preventDefault();
+	this.setAttribute("fill","green");
+}
+var drawDot = function(x,y)
+{
+	var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
+	c.setAttribute("cx",x)
+	c.setAttribute("cy",y)
+	c.setAttribute("r","10")
+	c.setAttribute("fill","red")
+	pic.appendChild(c)
+};
+var clicked = function(e)
+{
+	if (e.toElement == this)
+	{
+		drawDot(e.offsetX, e.offsetY)
+	}
+	else
+	{
+		drawDot(e.offsetX,e.offsetY)
+	}
+};
+pic.addEventListener("click",clicked)
